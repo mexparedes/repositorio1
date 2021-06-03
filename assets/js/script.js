3a7235f5882bd202537837fe8f2b655fe891db37
mexparedes <maxi.paredes.r@gmail.com>
2021-06-02 21:08:49 -0400

    primero1

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $("a").click(function(event){
+    event.preventDefault();
+    var gato = this.hash;
+
+    $("html").animate({
+      scrollTop: $(gato).offset().top
+    }, 800
+  )
+  });
+  $('[data-toggle="tooltip"]').tooltip();
+
+});
